e6a1fa5f46c36b8e0c960f663bf6e51583f708fb
cbk914 <cbk914@riseup.net>
2023-09-26 16:59:53 +0200

    Update email-analyzer.py

--- email-analyzer.py
+++ email-analyzer.py
@@ -1,6 +1,58 @@
+import os
+from dotenv import load_dotenv
 import argparse
 from email import message_from_string
 from email.header import decode_header
+
+# Load API keys from .env file
+load_dotenv()
+
+# Initialize API keys
+API_KEYS = {
+    'HAVEIBEENPWNED': os.getenv("HAVEIBEENPWNED_API_KEY"),
+    'CRIMINAL_IP': os.getenv("CRIMINAL_IP_API_KEY"),
+    'SHODAN': os.getenv("SHODAN_API_KEY"),
+    'CENSYS': os.getenv("CENSYS_API_KEY"),
+    'VIRUSTOTAL': os.getenv("VIRUSTOTAL_API_KEY"),
+    'HUNTER': os.getenv("HUNTER_API_KEY"),
+    'IPINFO': os.getenv("IPINFO_API_KEY"),
+    'FULLCONTACT': os.getenv("FULLCONTACT_API_KEY"),
+    'OPENWEATHERMAP': os.getenv("OPENWEATHERMAP_API_KEY")
+}
+
+
+
+def make_api_request(service, query):
+    if not API_KEYS.get(service):
+        print(f"Skipping {service} as API key is not provided.")
+        return None
+    
+    if not query:
+        print(f"Skipping {service} as query is empty.")
+        return None
+    
+    try:
+        if service == 'HAVEIBEENPWNED':
+            return haveibeenpwned(query)
+        elif service == 'CRIMINAL_IP':
+            return criminal_ip(query)
+        elif service == 'SHODAN':
+            return shodan(query)
+        elif service == 'CENSYS':
+            return censys(query)
+        elif service == 'VIRUSTOTAL':
+            return virustotal(query)
+        elif service == 'HUNTER':
+            return hunter(query)
+        elif service == 'IPINFO':
+            return ipinfo(query)
+        elif service == 'FULLCONTACT':
+            return fullcontact(query)
+        elif service == 'OPENWEATHERMAP':
+            return openweathermap(query)
+    except Exception as e:
+        print(f"An error occurred while querying {service}: {e}")
+        return None
 
 def parse_email(raw_email):
     email_data = {}
@@ -49,12 +101,27 @@ def main():
     with open(args.input, 'r', encoding='utf-8') as f:
         raw_emails = f.read()
     
-    # Assuming each email is separated by '---END OF EMAIL---'
     emails = raw_emails.split('---END OF EMAIL---')
     
     for email in emails:
         email_data = parse_email(email.strip())
         html_summary = generate_html_summary(email_data)
+        
+        # Make API requests
+        if email_data.get('From'):
+            email_from = email_data.get('From').split('<')[-1].split('>')[0]
+            haveibeenpwned_data = make_api_request('HAVEIBEENPWNED', email_from)
+            hunter_data = make_api_request('HUNTER', email_from)
+            fullcontact_data = make_api_request('FULLCONTACT', email_from)
+        
+        if email_data.get('Received-SPF'):
+            ip_from_spf = email_data.get('Received-SPF').split(' ')[-1]
+            criminal_ip_data = make_api_request('CRIMINAL_IP', ip_from_spf)
+            shodan_data = make_api_request('SHODAN', ip_from_spf)
+            censys_data = make_api_request('CENSYS', ip_from_spf)
+            virustotal_data = make_api_request('VIRUSTOTAL', ip_from_spf)
+            ipinfo_data = make_api_request('IPINFO', ip_from_spf)
+        
         print(html_summary)
 
 if __name__ == "__main__":
